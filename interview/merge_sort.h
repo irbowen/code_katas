861ac00e819bed49f4afcc1130c50bfc849badb6
Isaac Bowen <irbowen@umich.edu>
2015-07-28 19:52:10 -0500

    Work on merge sort, various cleanup and fixes

--- interview/merge_sort.h
+++ interview/merge_sort.h
@@ -1,22 +1,25 @@
 #include <algorithm>
 #include <iostream>
 
-template <typename T>
-void mergeSort(std::vector<T> v) {
-  mergeSort_1(v, 0, v.size()));
-}
 
-template <typename T>
-void mergeSort_1(std::vector<T>& v, int l, int r) {
-  if (l < r) {
-    auto m = (r+1) / 2;
+//template <typename T>
+//void mergeSort_1(std::vector<T>& v, int l, int r) {
+void mergeSort_1(std::vector<double>& v, int l, int r) {
+  if (r-l > 1) {
+    auto m = (r + l) / 2;
     mergeSort_1(v, l, m);
-    mergeSort_1(v, m+1, r);
-    merge(v, l, m, r);
+    mergeSort_1(v, m, r);
+    inplace_merge(v.begin()+l, v.begin()+m, v.begin()+r);
   }
 }
 
 template <typename T>
-void merge_1(std::vector<T> l, std::vector<T> r) {
+void merge_1(std::vector<T> v, std::vector<T> r) {
 
+}
+
+//template <typename T>
+//void mergeSort(std::vector<T> v) {
+void mergeSort(std::vector<double> v) {
+  mergeSort_1(v, 0, v.size());
 }
--- interview/sort.cpp
+++ interview/sort.cpp
@@ -4,16 +4,20 @@
 #include <ctime>
 #include <algorithm>
 #include <functional>
+#include "merge_sort.h"
+
+using namespace std;
+
 
 template <typename T>
-void print_vector(std::vector<T> v) {
+void print_vector(vector<T>& v) {
   for (auto x : v) {
-    std::cout << x << " ";
+    cout << x << " ";
   }
-  std::cout << std::endl;
+  cout << endl;
 }
 
-void selection_sort_vector(std::vector<int>& v) {
+void selection_sort_vector(vector<double>& v) {
   for (auto i = 0; i < v.size(); i++) {
     auto min_value = v.at(i);
     auto min_index = i;
@@ -23,24 +27,24 @@ void selection_sort_vector(std::vector<int>& v) {
         min_index = j;
       }
     }
-    std::swap(v.at(i), v.at(min_index));
+    swap(v.at(i), v.at(min_index));
   }
 }
 
-void bubble_sort_vector(std::vector<int>& v) {
+void bubble_sort_vector(vector<double>& v) {
   bool swaps = true;
   while (swaps) {
     swaps = false;
     for (auto j = 0; j < v.size()-1; j++) {
       if (v.at(j) < v.at(j+1)) {
-        std::swap(v.at(j), v.at(j+1));
+        swap(v.at(j), v.at(j+1));
         swaps = true;
       }
     }
   }
 }
 
-void insertion_sort_internet(std::vector<int>& v){
+void insertion_sort_internet(vector<double>& v){
   int j, temp;
   for (int i = i; i < v.size(); i++){
     j = i;
@@ -53,52 +57,63 @@ void insertion_sort_internet(std::vector<int>& v){
   }
 }
 
-void insertion_sort_vector(std::vector<int>& v) {
+void insertion_sort_vector(vector<double>& v) {
     for (auto i = 0; i < v.size(); i++) {
       auto j = i;
       while (j > 0 && v[j] < v[j-1]) {
-        std::swap(v[j], v[j-1]);
+        swap(v[j], v[j-1]);
+        j--;
       }
     }
 }
 
-void insertion_sort(std::vector<int> v) {
-  for (auto i = begin(v); i != end(v); ++i)
-    std::rotate(std::upper_bound(begin(v), i, *i), i, std::next(i));
+void std_sort(vector<double>& v) {
+  std::sort(v.begin(), v.end());
 }
 
-auto timeSize(int size, std::function<void(std::vector<int>&)> sort_type) {
-  std::vector<int> v(size);
-  std::srand(std::time(0));
+void insertion_sort(vector<double>& v) {
+  for (auto i = begin(v); i != end(v); ++i)
+    rotate(upper_bound(begin(v), i, *i), i, next(i));
+}
+
+auto timeSize(int size, function<void(vector<double>&)> sort_type) {
+  vector<double> v(size);
+  srand(time(0));
   for (auto& x : v) {
-    x = std::rand() % (size * size);
+    x = rand() % (size * 5);
   }
-  std::clock_t start;
-  start = std::clock();
+  clock_t start;
+  start = clock();
   sort_type(v);
-  auto duration = (std::clock() - start)  / (double)(CLOCKS_PER_SEC / 1000);
-  std::cout << " Time: " << duration << " ms";
+  auto duration = (clock() - start)  / (double)(CLOCKS_PER_SEC / 1000);
   return duration;
 }
 
 int main() {
-  for (int i = 2; i < 60000; i = i * 2 ) {
-    std::cout << "Size: " << i << " ";
-    std::cout << "\tBubble sort: ";
-    double f = timeSize(i, bubble_sort_vector);
-    std::cout << "\tSlection sort: ";
-    double s = timeSize(i, selection_sort_vector);
-    std::cout << "\t\tFactor: " << f/s << " " << std::endl;
-  }
-  for (int i = 2; i < 60000; i = i * 2 ) {
-    std::cout << "Size: " << i << " ";
-    std::cout << "\tInsertion STL sort: ";
-    double f = timeSize(i, insertion_sort);
-    std::cout << "\tInsertion Loop and swap sort: ";
-    double s = timeSize(i, insertion_sort_vector);
-    std::cout << "\tInsertion Internet and swap sort: ";
-    double t = timeSize(i, insertion_sort_internet);
-    std::cout << "\t\tFactor: " << f/s << " " << std::endl;
+  for (int i = 2; i < 480000000; i = i * 2 ) {
+    cout << "Size: " << i << " ";
+    //cout << "\tBubble: ";
+    //double bubble_sort_time = timeSize(i, bubble_sort_vector);
+    //cout << bubble_sort_time;
+    //cout << "\t\tSelection: ";
+    //double selection_sort_time = timeSize(i, selection_sort_vector);
+    //cout << selection_sort_time;
+     //cout << "\t\tInsertion STL: ";
+    //double insertion_stl_time = timeSize(i, insertion_sort);
+    //cout << insertion_stl_time;
+    //cout << "\t\tInsertion Isaac: ";
+    //double insertion_isaac_time = timeSize(i, insertion_sort_vector);
+    //cout << insertion_isaac_time;
+    //cout << "\t\tInsertion Internet: ";
+    //double insertion_internet_time = timeSize(i, insertion_sort_internet);
+    //cout << insertion_internet_time;
+    cout << "\t\t\tsort():";
+    double std_sort_time = timeSize(i, std_sort);
+    cout << std_sort_time;
+    cout << "\t\tMergeSort():";
+    double merge_sort_time = timeSize(i, mergeSort);
+    cout << merge_sort_time;
+    cout <<  endl;
   }
 
   return 0;
