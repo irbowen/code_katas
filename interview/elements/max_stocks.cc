598d590ac93af45cedddec9e85db9711bba3180d
Isaac Bowen <irbowen@umich.edu>
2015-09-27 18:36:22 -0400

    test for max stocks

---
+++ interview/elements/max_stocks.cc
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+void maxStockProfit(std::vector<double> v) {
+  double min_value = std::numeric_limits<double>::max();
+  double max_profit = 0;
+  for (auto& item : v) {
+    min_value = std::min(min_value, item);
+    max_profit = std::max(max_profit, item - min_value);
+  }
+  std::cout << "Min value is " << min_value << " Max profit is " << max_profit << std::endl;
+}
+
+int main void() {
+  std::vector<double>prices = {4, 1, 2, 3, 4, 5, 2, 0, 2, 4, 8}
+}
